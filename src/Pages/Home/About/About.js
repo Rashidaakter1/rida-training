94678192a6bdbf5abd73fa7d338dcbdb4177daca
Rashidaakter1 <rashidaakterchadni@gmail.com>
2022-04-17 23:10:27 +0600

    about me section

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -3,7 +3,16 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>about</h1>
+            <h1>About Me</h1>
+            <h1>Rashida Akter</h1>
+            <img src="" alt="" />
+            <h5>
+                My goal is to become a front-end developer.I really wanna know that how a website works .How developer interacts with the user using web-languages. In that process recently I have learnt html,css,javascript,dom manipulation,es6 and many more.I am working very hard to get what I want.Practising daily for 5-6 hours.Using external sources for assignments or projects.Focused on projects, how can I make that better for user.It's really hard to get from google or any other resources but I don't give up .Also get motivated by our course instructor that 
+                <blockquote>"No pain ,No Gain"</blockquote>
+                that's true.So , I Won't give up on my dreams.No matter How hard I have to work. 
+
+            </h5>
+
         </div>
     );
 };
